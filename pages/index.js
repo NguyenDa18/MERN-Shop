fac6340251c828a885e0ad860cb58709fb89a62d
Danh Nguyen <danhnguyen267@gmail.com>
2019-09-29 17:40:01 -0700

    Fetching products on server side

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home() {
+import React from 'react'
+import axios from 'axios'
+
+const Home = ({ products }) => {
   return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+  const url = `http://localhost:3000/api/products`
+  const response = await axios.get(url)
+  return { products: response.data }
 }
 
 export default Home;
