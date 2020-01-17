3fbfac5bfb11bca2b8c3ae5281159a6467e9b0e9
Danh Nguyen <danhnguyen267@gmail.com>
2020-01-16 23:35:31 -0800

    Created OrderItem component for account orders to map over

--- components/Account/AccountOrders.js
+++ components/Account/AccountOrders.js
@@ -1,15 +1,24 @@
-import { Header, Accordion, Label, Segment, Icon, Button, List, Image } from 'semantic-ui-react'
+import {
+  Header,
+  Accordion,
+  Label,
+  Segment,
+  Icon,
+  Button,
+  List
+} from "semantic-ui-react";
+import OrderItem from './OrderItem'
+import { useRouter } from "next/router";
 import formatDate from '../../utils/formatDate'
-import { useRouter } from 'next/router'
 
-const AccountOrders = ({ orders }) => {
-  const router = useRouter()
+function AccountOrders({ orders }) {
+  const router = useRouter();
 
-  const mapOrdersToPanels = orders => {
+  function mapOrdersToPanels(orders) {
     return orders.map(order => ({
       key: order._id,
       title: {
-        content: <Label color="blue" ribbon content={formatDate(order.createdAt)} /> 
+        content: <Label color="blue" content={formatDate(order.createdAt)} />
       },
       content: {
         content: (
@@ -24,23 +33,10 @@ const AccountOrders = ({ orders }) => {
                 style={{ marginLeft: "1em" }}
               />
             </List.Header>
-            <List divided relaxed>
-              {order.products.map((p, index) => (
-                <List.Item key={index}>
-                  <Image avatar src={p.product.mediaUrl} />
-                  <List.Content>
-                    <List.Header>
-                      {p.product.name}
-                    </List.Header>
-                    <List.Description>
-                      {p.quantity} · ${p.product.price}
-                    </List.Description>
-                  </List.Content>
-                  <List.Content floated="right">
-                    <Label tag color="red" size="tiny">
-                      {p.product.sku}
-                    </Label>
-                  </List.Content>
+            <List>
+              {order.products.map((orderProduct, idx) => (
+                <List.Item key={idx}>
+                  <OrderItem orderProduct={orderProduct} />
                 </List.Item>
               ))}
             </List>
@@ -52,25 +48,30 @@ const AccountOrders = ({ orders }) => {
 
   return (
     <>
-    <Header as="h2">
-      <Icon name="shopping bag" />
-      Order History
-    </Header>
-    {orders.length === 0 ? (
-      <Segment inverted tertiary color="grey" textAlign="center">
-        <Header icon>
-          <Icon name="copy outline" />
-          No past orders.
-        </Header>
-        <div>
-          <Button onClick={() => router.push("/")} color="orange">
-            View Products
-          </Button>
-        </div>
-      </Segment>
-    ) : (
-      <Accordion fluid styled exclusive={false} panels={mapOrdersToPanels(orders)} />
-    )}
+      <Header as="h2">
+        <Icon name="folder open" />
+        Order History
+      </Header>
+      {orders.length === 0 ? (
+        <Segment inverted tertiary color="grey" textAlign="center">
+          <Header icon>
+            <Icon name="copy outline" />
+            No past orders.
+          </Header>
+          <div>
+            <Button onClick={() => router.push("/")} color="orange">
+              View Products
+            </Button>
+          </div>
+        </Segment>
+      ) : (
+        <Accordion
+          fluid
+          styled
+          exclusive={false}
+          panels={mapOrdersToPanels(orders)}
+        />
+      )}
     </>
   );
 }
