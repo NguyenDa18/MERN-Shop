12e30521193ed151070d51e9cac105893ee5baa6
Danh Nguyen <danhnguyen267@gmail.com>
2020-07-20 16:40:35 -0700

    Changed prod url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://mern-shop-git-master.danhnguyen267.vercel.app' : 'http://localhost:3000';
 
 export default baseUrl;
